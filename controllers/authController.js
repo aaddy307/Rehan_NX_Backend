75062bf00555b76abd32bbbea9bae2af91f41683
aaddy307 <aaddy.ly143@gmail.com>
2026-06-04 15:56:03 +0530

    fix: update cookie settings for login to enhance security and compatibility

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,8 +30,8 @@ export const login = async (req, res, next) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
